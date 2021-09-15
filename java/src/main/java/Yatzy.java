f6968ec4d50451f3db9b59fda4a6c65a357607f4
Guillaume Berto <guillaume.berto-extern@renault.com>
2021-09-15 03:28:35 +0200

    Yatzy Kata Refactoring

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -1,241 +1,138 @@
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
-    {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+    public static int chance(int d1, int d2, int d3, int d4, int d5) {
+        return d1 + d2 + d3 + d4 + d5;
     }
 
-    public static int yatzy(int... dice)
-    {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die-1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
-        return 0;
+    public static int yatzy(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Integer> occurrenceOfDiceMap = getOccurrenceOfDiceMap(d1, d2, d3, d4, d5);
+        if (occurrenceOfDiceMap.size() != 1)
+            return 0;
+        return 50;
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
+        return Arrays.stream(new int[]{d1, d2, d3, d4, d5}).filter(d -> d == 1).reduce(Integer::sum).orElse(0);
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
+        return Arrays.stream(new int[]{d1, d2, d3, d4, d5}).filter(d -> d == 2).reduce(Integer::sum).orElse(0);
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
+        return Arrays.stream(new int[]{d1, d2, d3, d4, d5}).filter(d -> d == 3).reduce(Integer::sum).orElse(0);
     }
 
-    protected int[] dice;
-    public Yatzy(int d1, int d2, int d3, int d4, int _5)
-    {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
+    public static int fours(int d1, int d2, int d3, int d4, int d5) {
+        return Arrays.stream(new int[]{d1, d2, d3, d4, d5}).filter(d -> d == 4).reduce(Integer::sum).orElse(0);
     }
 
-    public int fours()
-    {
-        int sum;    
-        sum = 0;
-        for (int at = 0; at != 5; at++) {
-            if (dice[at] == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
+    public static int fives(int d1, int d2, int d3, int d4, int d5) {
+        return Arrays.stream(new int[]{d1, d2, d3, d4, d5}).filter(d -> d == 5).reduce(Integer::sum).orElse(0);
     }
 
-    public int fives()
-    {
-        int s = 0;
-        int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
-                s = s + 5;
-        return s;
+    public static int sixes(int d1, int d2, int d3, int d4, int d5) {
+        return Arrays.stream(new int[]{d1, d2, d3, d4, d5}).filter(d -> d == 6).reduce(Integer::sum).orElse(0);
     }
 
-    public int sixes()
-    {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
+    public static int score_pair(int d1, int d2, int d3, int d4, int d5) {
+        final List<Integer> multipleOccurrenceList = getDiceWithSpecificOccurrence(occurrenceOfDice -> occurrenceOfDice.getValue() >= 2,
+            d1, d2, d3, d4, d5);
+        return 2 * multipleOccurrenceList.stream()
+            .max(Comparator.naturalOrder()).orElse(0);
     }
 
-    public static int score_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
-        return 0;
-    }
+    public static int two_pair(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Integer> occurrenceOfDiceMap = getOccurrenceOfDiceMap(d1, d2, d3, d4, d5);
 
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
-                n++;
-                score += (6-i);
-            }        
-        if (n == 2)
-            return score * 2;
-        else
+        final Integer diceWith4OccurrenceAtLeast = occurrenceOfDiceMap.entrySet().stream()
+            .filter(occurrenceOfDice -> occurrenceOfDice.getValue() >= 4)
+            .map(Map.Entry::getKey)
+            .findFirst().orElse(0);
+
+        if (diceWith4OccurrenceAtLeast != 0)
+            return 4 * diceWith4OccurrenceAtLeast;
+
+        final List<Integer> diceWith2Or3OccurrenceList = occurrenceOfDiceMap.entrySet().stream()
+            .filter(occurrenceOfDice -> occurrenceOfDice.getValue() >= 2)
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toList());
+
+        if (diceWith2Or3OccurrenceList.size() != 2)
             return 0;
+        return 2 * diceWith2Or3OccurrenceList.stream().reduce(Integer::sum).get();
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
-    }
-
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
-    }
-
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
-    }
-
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
-    }
-
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
+    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+        final List<Integer> multipleOccurrenceList = getDiceWithSpecificOccurrence(occurrenceOfDice -> occurrenceOfDice.getValue() >= 3,
+            d1, d2, d3, d4, d5);
+        if (multipleOccurrenceList.size() != 1)
             return 0;
+        return 3 * multipleOccurrenceList.get(0);
+    }
+
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+        final List<Integer> multipleOccurrenceList = getDiceWithSpecificOccurrence(occurrenceOfDice -> occurrenceOfDice.getValue() >= 4,
+            d1, d2, d3, d4, d5);
+        if (multipleOccurrenceList.size() != 1)
+            return 0;
+        return 4 * multipleOccurrenceList.get(0);
+    }
+
+    public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
+        final List<Integer> diceWithOneOccurrenceList = getDiceWithSpecificOccurrence(occurrenceOfDice -> occurrenceOfDice.getValue() == 1,
+            d1, d2, d3, d4, d5);
+
+        if (diceWithOneOccurrenceList.size() != 5 || diceWithOneOccurrenceList.stream().reduce(Integer::sum).orElse(0) != 15)
+            return 0;
+        return 15;
+    }
+
+    public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
+        final List<Integer> diceWithOneOccurrenceList = getDiceWithSpecificOccurrence(occurrenceOfDice -> occurrenceOfDice.getValue() == 1,
+            d1, d2, d3, d4, d5);
+
+        if (diceWithOneOccurrenceList.size() != 5 || diceWithOneOccurrenceList.stream().reduce(Integer::sum).orElse(0) != 20)
+            return 0;
+        return 20;
+    }
+
+    public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Integer> occurrenceOfDiceMap = getOccurrenceOfDiceMap(d1, d2, d3, d4, d5);
+
+        final Integer diceWith2Occurrence = occurrenceOfDiceMap.entrySet().stream()
+            .filter(occurrenceOfDice -> occurrenceOfDice.getValue() == 2)
+            .map(Map.Entry::getKey)
+            .findFirst().orElse(0);
+
+        final Integer diceWith3Occurrence = occurrenceOfDiceMap.entrySet().stream()
+            .filter(occurrenceOfDice -> occurrenceOfDice.getValue() == 3)
+            .map(Map.Entry::getKey)
+            .findFirst().orElse(0);
+
+        if (diceWith2Occurrence == 0 || diceWith3Occurrence == 0)
+            return 0;
+
+        return 2 * diceWith2Occurrence + 3 * diceWith3Occurrence;
+    }
+
+    private static List<Integer> getDiceWithSpecificOccurrence(Predicate<Map.Entry<Integer, Integer>> entryPredicate,
+                                                               int d1, int d2, int d3, int d4, int d5) {
+        Map<Integer, Integer> occurrenceOfDiceMap = getOccurrenceOfDiceMap(d1, d2, d3, d4, d5);
+        return occurrenceOfDiceMap.entrySet().stream()
+            .filter(entryPredicate)
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toList());
+    }
+
+    private static Map<Integer, Integer> getOccurrenceOfDiceMap(int d1, int d2, int d3, int d4, int d5) {
+        final List<Integer> diceRolls = Arrays.asList(d1, d2, d3, d4, d5);
+        Map<Integer, Integer> occurrenceOfDiceMap = new HashMap<>();
+        diceRolls.stream().distinct().forEach(d -> occurrenceOfDiceMap.put(d, Collections.frequency(diceRolls, d)));
+        return occurrenceOfDiceMap;
     }
 }
-
-
-
